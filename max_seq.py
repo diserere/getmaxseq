b70695b7da2fed26f9e32dddce7efadb1454e0ca
Diserere <diserere@ya.ru>
2019-03-02 20:24:55 +0300

    Edit docstrings, error messages and comments

--- max_seq.py
+++ max_seq.py
@@ -31,16 +31,19 @@ __version__ = '1.1.3'
 
 def is_matching(char, n, charmap, charmap_len):
     """
-    TODO: DOCSTRING
-    args:
-        char: str
-        1-length str to operate
-        n: int
-        current pos in str to save as last occurence
-        charmap: dict
-        dict with char as key and n as value, 
-         like {char: n[, char: n]...}
-        charmap_len: int
+    Matches given char with keys in dict, update value by n 
+        if found, or add {key: value} if dict length is less
+        than max length, of returns False.
+        
+    Args:
+        str char:
+            one-char string to match
+        int n: 
+            current pos in string to save as last occurence
+        dict charmap: 
+            dict with char as key and n as value
+        int charmap_len: 
+            max num of elements
     """
     # try to match with stack
     if char in charmap or len(charmap) < charmap_len:
@@ -53,7 +56,8 @@ def is_matching(char, n, charmap, charmap_len):
 
 
 def save_max_substr(cur_substr, max_substr):
-    if len(cur_substr) >= len(max_substr): # Use ">" here to save first max substr of the same length instead of last one
+    # Use ">" here to save first max substr found instead of last one
+    if len(cur_substr) >= len(max_substr):
         max_substr = cur_substr
     return max_substr
 
@@ -61,18 +65,21 @@ def save_max_substr(cur_substr, max_substr):
 def get_max_seq(string, charset_len):
     """ 
     Return longest substring consisting of not more than given number of
-    different chars found in input string. If more than one string of
-    equal length is found then the last one is returned.
+        different chars found in input string. If more than one string of
+        equal length is found then the last one is returned.
+        
     Args:
-        str string: input string
-        int charset_len: max number of different chars in sequence
+        str string: 
+            input string
+        int charset_len:
+            max number of different chars in sequence, expected to be >=0
     """
     
     # charset_len should be int and positive
     if type(charset_len) is not int:
-        raise TypeError('arg charset_len: should be int but unexpected type: %s' % type(charset_len) )
+        raise TypeError('int charset_len: unexpected type: %s' % type(charset_len) )
     elif charset_len < 0:
-        raise TypeError('arg charset_len: should not be negative but is: %s' % str(charset_len) )
+        raise TypeError('charset_len: should not be negative: %s' % str(charset_len) )
     
     # Null charset_len cause exception while pop from null list, see below
     if len(string) == 0 or charset_len == 0:
@@ -86,13 +93,14 @@ def get_max_seq(string, charset_len):
     
     while p <= len(string)-1:
         cur_char = string[p]
-        if not is_matching(cur_char, p, charset, charset_len): # end of sequence is found
-            # compare and save result 
+        if not is_matching(cur_char, p, charset, charset_len): 
+            # end of sequence is found; compare and save result 
             max_substr = save_max_substr(cur_substr, max_substr) 
-            # exclude first element from charmap, 
-            #  get abs new start address (+1) from its address
+            # get key of leftmost (by addr) element from charmap
             min_key = min(charset, key=charset.get)
+            # get new start address from its address
             new_start_addr = charset[min_key] - p + len(cur_substr) + 1
+            # remove leftmost element
             charset.pop(min_key)
             # add new char to charmap
             charset[cur_char] = p
